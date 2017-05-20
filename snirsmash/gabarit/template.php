082481c4dfa39e86dd5ba1f8d5f21d779cc9c515
Leo <Leo@192.168.1.6>
2017-05-21 01:05:38 +0200

    Ajout de « display : flex; justify-content : center » dans la propriété CSS #divZoneVS et réduction des margins de #imgVS
    Création des liens (NavBar) avec transfert de données (idCat, nomCat et adjCat). 
    Modification du nom de la catégorie + de la catchphrase (« Le développeur le plus … ») suite à un clic sur l’un de ces liens, méthode bizarre, dis-moi ce que tu en penses.
    
    + clic sur le logo = retour sur la page « humour » (qui fait un peu office de page d’accueil pour le moment)
    
    + j’ai essayé de placer les flèches centrées : 420/2 (hauteur de la div divisée par 2) - 30/2 (hauteur de la flèche divisée par 2) -> top : 195px :)
    
    +ajout de la bibliothèque jQuery et de la page qu’on va utiliser pour nos scripts (intégration sur le template aussi du coup)

--- snirsmash/gabarit/template.php
+++ snirsmash/gabarit/template.php
@@ -22,24 +22,42 @@ and open the template in the editor.
     </head>
     <body>
         <div id="conteneur">
-            <header><img id="header" src="img/headersmash.png"></header>
+            <header><a href='index.php?idCat=1&nomCat=Humour&adjCat=drôle'><img id="header" src="img/headersmash.png"></a></header>
             <!--requête pour la NavBar-->
             <?php
-            $reponse = $objBdd->query('SELECT idCat, nomCat FROM categories ORDER BY idCat ASC');
+            $reponse = $objBdd->query('SELECT idCat, nomCat, adjCat FROM categories ORDER BY idCat ASC');
             ?>
 
             <nav id="idNav">
                 <ul>
+                    <!--Récupération de la NavBar sur la bdd + affectation des liens
+                                        avec l'id de la catégorie-->
                     <?php
                     foreach ($reponse as $resultat) {
                         ?>
-                        <li><?php echo $resultat["nomCat"];
-                }
-                    ?></li>
+                        <li><a href="index.php?idCat=<?php echo $resultat["idCat"]; ?>&nomCat=<?php echo $resultat["nomCat"]; ?>&adjCat=<?php echo $resultat["adjCat"] ?>" >
+                                <?php echo $resultat["nomCat"]; ?></a>
+                            <?php
+                        }
+                        ?></li>
                 </ul>
             </nav>
-            <h1 id="idTitreCategorie">Humour</h1>
-            <p id="idCatchphrase">Qui est le développeur le plus drôle ?</p>
+
+            <?php
+//            if (isset($_GET["nomCat"])) {
+//                $nomCat = strval($_GET["nomCat"]);
+//            }
+            ?>
+            <!--Récupère et affiche le nom de la catégorie-->
+            <h1 id="idTitreCategorie"><?php echo $_GET["nomCat"]; ?></h1>
+
+            <?php
+//            if (isset($_GET["adjCat"])) {
+//                $adjCat = strval($_GET["adjCat"]);
+//            }
+            ?>
+            <!--Récupère et affiche l'adj de la catégorie-->
+            <p id="idCatchphrase">Qui est le développeur le plus <?php echo $_GET["adjCat"]; ?> ?</p>
             <div id="divZoneVS">
                 <div id="divGauche"><img id="imgFlecheGch" src="img/flechegauchesmash.png"></div>
                 <div id="imgVS"><img src="img/vssmash.png"></div>
@@ -49,5 +67,7 @@ and open the template in the editor.
             echo $contenu;
             ?>
         </div>
+        <script src="js/jquery-3.2.0.js" type="text/javascript"></script>
+        <script src="js/monScript.js" type="text/javascript"></script>
     </body>
 </html>
